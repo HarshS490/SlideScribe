8aa597039d3f51569a097700d09fd02dd13bf4ba
Harsh Singh <harsh.singh30112004@gmail.com>
2025-01-22 23:21:21 +0530

    wired up middleware logic and improved login form

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,25 @@
-import {withAuth} from 'next-auth/middleware';
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
 
-export default withAuth({
-    pages:{
-        signIn:"/login"
+export default withAuth(function middleware(req) {
+  if(req.nextUrl.pathname.startsWith("/login") && req.nextauth.token!==null){
+    return NextResponse.redirect(new URL("/explore", req.url));
+  }
+  if((req.nextUrl.pathname.startsWith("/login") || req.nextUrl.pathname.startsWith("/users")) && req.nextauth.token === null){
+    return NextResponse.redirect(new URL("/login",req.url));
+  }
+  
+}, {
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async authorized({ token }) {
+      return !!token;
     },
-    callbacks:{
-        async authorized({token}){
-            if(!!token){
-                console.log('authenticated');
-            }
-            else{
-                console.log('unauthenticated');
-            }
-
-            return !!token;
-        },
-    },
+  },
 });
 
-
-
-
 export const config = {
-    matcher:[
-        "/dashboard/:path*",
-        "/conversations/:path*",
-        
-    ]
-}
+  matcher: ["/dashboard/:path*"],
+};
